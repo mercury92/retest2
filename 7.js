6b0905a180d8bc9f4c5beb55e0f64bdff2d7668e
mercury92 <pisanu.prm@gmail.com>
2020-06-30 22:15:14 +0700

    2

--- 7.js
+++ 7.js
@@ -1,23 +1,22 @@
 const findGDC = (n) => {
-    let sum = []
+    let sum
     let c = 10
-    let divisible
-    let noDivisible = []
+    let divisible = 0
     const max = Math.max.apply(Math, n)
 
-    for (i = n.length - 1; i >= 0; i--) {
-        for (j = 2; j < max; j++) {
-            divisible = ''
-            if (n[i] % j !== 0) {
-                divisible = 1
-                noDivisible = j
+    for (i = 2; i < max; i++) {
+        divisible = 0
+        for (j = 0; j < n.length; j++) {
+            if (n[j] % i == 0) {
+                divisible += 1
             }
-            if (divisible !== 1) {
-                sum = sum+j
+            if (divisible == n.length) {
+                sum = i
             }
         }
     }
+
     return sum
 }
 
-console.log(findGDC([6, 8, 16]))
+console.log(findGDC([12,18]))
--- 8.js
+++ 8.js
@@ -1,3 +1,23 @@
-let findCR =() => {
-    for()
+const findLCM = (n) => {
+    let sum
+    let c = 10
+    let divisible = 0
+    const max = Math.max.apply(Math, n)
+
+    for (i = max; i < 500; i++) {
+        divisible = 0
+        for (j = 0; j < n.length; j++) {
+            if ( i%n[j]  === 0) {
+                divisible += 1
+            }
+        }
+        if (divisible === n.length) {
+            sum = i
+            break;
+        }
+    }
+    
+    return sum
 }
+
+console.log(findLCM([2,3,5,7]))
